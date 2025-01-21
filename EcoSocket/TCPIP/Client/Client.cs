26e57795319da54cdecc64453d15b73194c9dd02
leejm\leejm <c__o_k__@naver.com>
2025-01-22 00:33:37 +0900

    eco server 실습

---
+++ EcoSocket/TCPIP/Client/Client.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.123.100"), 20000);
+                socket.Connect(endPoint);
+
+                while (true)
+                {
+                    string str = Console.ReadLine();
+                    if (str == "exit")
+                    {
+                        return;
+                    }
+
+                    byte[] strBuffer = Encoding.UTF8.GetBytes(str);
+                    byte[] newBuffer = new byte[2 + strBuffer.Length];
+                    byte[] dataSize = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)strBuffer.Length));
+                    
+                    Array.Copy(dataSize, 0, newBuffer, 0, dataSize.Length);
+                    Array.Copy(strBuffer, 0, newBuffer, 2, strBuffer.Length);
+                    
+                    socket.Send(newBuffer);
+
+                    /*
+                    byte[] buffer2 = new byte[256];
+                    int bytesRead = socket.Receive(buffer2);
+                    if (bytesRead < 1)
+                    {
+                        Console.WriteLine("서버의 연결 종료");
+                        return;
+                    }
+
+                    string str2 = Encoding.UTF8.GetString(buffer2);
+                    Console.WriteLine("받음 : " + str2);
+                    */
+                }
+            }
+        }
+    }
+}
---
+++ EcoSocket/TCPIP/Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal class Server
+{
+    static void Main(string[] args)
+    {
+        using (Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.123.100"), 20000);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(20);
+
+            using (Socket clientSocket = serverSocket.Accept())
+            {
+                Console.WriteLine(clientSocket.RemoteEndPoint);
+
+                while (true)
+                {
+                    byte[] headerBuffer = new byte[2];
+                    int n1 = clientSocket.Receive(headerBuffer);
+                    if (n1 < 1)
+                    {
+                        Console.WriteLine("클라이언트의 연결종료");
+                        return;
+                    }
+                    else if (n1 == 1)
+                    {
+                        clientSocket.Receive(headerBuffer,1,1,SocketFlags.None);
+                    }
+
+                    short dataSize = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(headerBuffer));
+                    byte[] dataBuffer = new byte[dataSize];
+
+                    int totalRecv = 0;
+
+                    while (totalRecv < dataSize)
+                    {
+                        int n2 = clientSocket.Receive(dataBuffer, totalRecv, dataSize - totalRecv, SocketFlags.None);
+                        totalRecv += n2;
+                    }
+
+                    string str = Encoding.UTF8.GetString(dataBuffer);
+                    Console.WriteLine(str);
+
+                    //clientSocket.Send(buffer);
+                }
+            }
+        }
+    }
+}
