89618c0d1ee7b4a7e8c589d1dbce37243c7f4f2a
leejm\leejm <c__o_k__@naver.com>
2025-01-24 23:30:22 +0900

    TAP 소켓

---
+++ TAP/Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client;
+
+internal class Client
+{
+    static async Task Main(string[] args)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.200.120"), 20000);
+
+        await socket.ConnectAsync(endPoint);
+
+        while (true)
+        {
+            string str = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            await socket.SendAsync(buffer, SocketFlags.None);
+        }
+    }
+}
---
+++ TAP/Server/Server.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal class Server
+{
+    static async Task Main(string[] args)
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.200.120"), 20000);
+        serverSocket.Bind(endPoint);
+        serverSocket.Listen(1000);
+
+        // TAP (작업 기반 비동기 패턴)
+
+        while (true)
+        {
+            Socket clientSocket = await serverSocket.AcceptAsync();
+            Console.WriteLine(clientSocket.RemoteEndPoint);
+            ThreadPool.QueueUserWorkItem(ReadAsync, clientSocket);
+        }
+    }
+
+    private static async void ReadAsync(object? sender)
+    {
+        Socket clientSocket = (Socket)sender;
+        while (true)
+        {
+            byte[] buffer = new byte[256];
+            int n1 = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+            if (n1 < 1)
+            {
+                Console.WriteLine("cliend disconnect");
+                clientSocket.Dispose();
+                return;
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(buffer));
+        }
+    }
+}
